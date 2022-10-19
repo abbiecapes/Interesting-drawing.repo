bfce049145324ba2b467503e886149916e747f1d
abbiecapes <amc20fe@fsu.edu>
2022-10-19 11:52:51 -0400

    colors

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
 }
 
 function draw() {
   background(125);
+  circle (250, 250, 300)
+  fill("yellow")
+  stroke("yellow");
+  circle (200, 200, 50)
+  fill("black")
+  circle (300, 200, 50)
+  arc(250, 300, 80, 80, 0, PI + QUARTER_PI, CHORD);
 }
